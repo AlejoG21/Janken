47fbd06cb4308aa43d8c9db749f15d49d06fceb1
NagoG21 <alejogoparra@gmail.com>
2020-11-08 15:24:23 -0300

    add new button-based game function.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,62 @@
+const body = document.querySelector("body");;
+const divBtn = document.createElement("div");
+const divRes = document.createElement("div");
+const btnRock = document.createElement("button");
+const btnPaper = document.createElement("button");
+const btnScissors = document.createElement("button");
+const scorePlayer = document.createElement("p");
+const scoreComputer = document.createElement("p");
+const scorePlayerT = document.createElement("p");
+const scoreComputerT = document.createElement("p");
+const rounds = document.createElement("p");
+const playA = document.createElement("p");
+const testbtn = document.querySelector("button");
+function assignr(name,id,parent,text){
+    name.setAttribute("id",id);
+    name.textContent = text;
+    parent.appendChild(name);
+}
+let roundsV= 1;
+assignr(divBtn,"divBtn",body);
+assignr(divRes,"divRes",body);
+assignr(btnRock,"Rock",divBtn,"Rock");
+assignr(btnPaper,"Paper",divBtn,"Paper");
+assignr(btnScissors,"Scissors",divBtn,"Scissors");
+assignr(scorePlayerT,"scorePlayerT",divRes,"Player:");
+assignr(scorePlayer,"scorePlayer",divRes,0);
+assignr(rounds,"rounds",divRes,`round ${roundsV}`);
+assignr(playA,"playA",divRes,"");
+assignr(scoreComputerT,"scoreComputerT",divRes,"Computer:");
+assignr(scoreComputer,"scoreComputer",divRes,0);
+
+
+btnRock.addEventListener("click", () =>{
+    if(rounds.textContent == "You won!" || rounds.textContent == "You lose!") {
+        scorePlayer.textContent = 0;
+        scoreComputer.textContent = 0;
+        roundsV = 1;
+        rounds.textContent = `round ${roundsV}`;
+        playA.textContent =" ";
+        janKenPon("rock");
+    }else {janKenPon("rock")}});
+btnPaper.addEventListener("click", ()=>{
+    if(rounds.textContent == "You won!" || rounds.textContent == "You lose!") {
+        scorePlayer.textContent = 0;
+        scoreComputer.textContent = 0;
+        roundsV = 1;
+        rounds.textContent = `round ${roundsV}`;
+        playA.textContent =" ";
+        janKenPon("paper");
+    }else {janKenPon("paper")}});
+btnScissors.addEventListener("click", ()=> {
+    if(rounds.textContent == "You won!" || rounds.textContent == "You lose!") {
+        scorePlayer.textContent = 0;
+        scoreComputer.textContent = 0;
+        roundsV = 1;
+        rounds.textContent = `round ${roundsV}`;
+        playA.textContent =" ";
+        janKenPon("scissors");
+    }else {janKenPon("scissors")}});
 function computerPlay() {
 function random() {
 return Math.floor(Math.random()*3+1)
@@ -17,18 +76,17 @@ switch (janK) {
         break;
     }
 }
-
-function janKenPon() {
-    let playerPick;  
-        function playerChoice(){
-            playerPick = prompt("rock,paper or scissors?","rock");
-            playerPick = playerPick.toLowerCase();
-            if(playerPick == "rock" || playerPick == "paper" || playerPick == "scissors"){
-                alert("good choice!"); return playerPick;} 
-            else{
-                alert("Please pick between 'rock', 'paper' or 'scissors'!"); playerChoice();}  
-        }
-    playerPick = playerChoice();
+function winCall(){
+    let player = parseInt(scorePlayer.textContent);
+    let computer = parseInt(scoreComputer.textContent);
+    if(roundsV == 6 || player == 3 || computer == 3){
+        if(player>computer){rounds.textContent = "You won!";
+    playA.textContent= "play again?";}
+        else{ rounds.textContent = "You lose!";
+    playA.textContent= "play again?";};
+    } else{ rounds.textContent = `round ${roundsV}`;}
+}
+function janKenPon(playerPick) { 
     let computerPick = computerPlay();
     if(playerPick == computerPick){ 
         alert(`it's a draw!, both picked ${playerPick}!`)
@@ -36,23 +94,23 @@ function janKenPon() {
         switch (playerPick) {
             case "rock":
                 if(computerPick == "scissors"){
-                    alert('you win! rock beats scissors!'); return 'you win! rock beats scissors!'} 
+                    alert('you win! rock beats scissors!'); scorePlayer.textContent++; roundsV++;winCall()} 
                 else {
-                    alert('you lose! paper beats rock!'); return 'you lose! paper beats rock!'} 
+                    alert('you lose! paper beats rock!'); scoreComputer.textContent++; roundsV++;winCall()} 
             break;
         
             case "paper":
                 if(computerPick == "rock"){
-                    alert('you win! paper beats rock!'); return 'you win! paper beats rock!'} 
+                    alert('you win! paper beats rock!'); scorePlayer.textContent++; roundsV++;winCall()} 
                 else {
-                    alert('you lose! scissors beats paper!'); return 'you lose! scissors beats paper!'}
+                    alert('you lose! scissors beats paper!'); scoreComputer.textContent++; roundsV++;winCall()}
             break;
             
             case "scissors":
                 if(computerPick == "paper"){
-                    alert('you win! scissors beats paper!'); return 'you win! scissors beats paper!'} 
+                    alert('you win! scissors beats paper!'); scorePlayer.textContent++; roundsV++; winCall()} 
                 else {
-                    alert('you lose! rock beats paper!'); return 'you lose! rock beats paper!'}
+                    alert('you lose! rock beats paper!'); scoreComputer.textContent++; roundsV++;winCall()}
             break;
             
             default:
@@ -60,56 +118,4 @@ function janKenPon() {
             break;
         }
     }
-}
-function game() {
-    let playerScore =0;
-    let computerScore =0;
-    let rounds=0;
-    while(rounds<5){
-        switch (janKenPon()) {
-            case 'you win! rock beats scissors!':
-                playerScore++;
-                rounds++;
-                break;
-        
-            case 'you lose! paper beats rock!':
-                computerScore++;
-                rounds++;
-                break;
-        
-            case 'you win! paper beats rock!':
-                playerScore++;
-                rounds++;
-                break;
-        
-            case 'you lose! scissors beats paper!':
-                computerScore++;
-                rounds++;
-                break;
-        
-            case 'you win! scissors beats paper!':
-                playerScore++;
-                rounds++;
-                break;
-        
-            case 'you lose! rock beats paper!':
-                computerScore++;
-                rounds++;
-                break;
-        
-            default:
-                
-                break;
-        }
-        console.log(playerScore);
-        if (playerScore>2) {
-            alert(`you win! ${playerScore} to ${computerScore}`); return `you win! ${playerScore} to ${computerScore}`
-        } 
-        else if (computerScore>2){
-            alert(`you lose! ${playerScore} to ${computerScore}`); return `you lose! ${playerScore} to ${computerScore}`
-        } else {
-            alert("next round!");
-        }
-    }
-    
 }
